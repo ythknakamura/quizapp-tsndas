b444a548cb0df8159f5dd4d8ec5603f7fc5d3d3a
ythknakamura <ythk.nakamura@gmail.com>
2024-08-12 16:04:02 +0900

    chap4まで

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<main class="flex flex-col h-svh">
+  <!-- メニューバー -->
+  <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
+      <div>スコア:0</div>
+      <div>連続正解数:0</div>
+  </div>
+  
+  <!-- 問題 -->
+  <div class="bg-green-200 text-center text-4xl py-4">鮪</div>
+  
+  <!-- 選択肢 -->
+  <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
+    <button class="bg-blue-500 text-blue-100 border-blue-800 text-lg font-bold border-2 rounded-xl py-4 w-4/5">まぐろ</button>
+    <button class="bg-blue-500 text-blue-100 border-blue-800 text-lg font-bold border-2 rounded-xl py-4 w-4/5">ぶり</button>
+    <button class="bg-blue-500 text-blue-100 border-blue-800 text-lg font-bold border-2 rounded-xl py-4 w-4/5">くじら</button>
+  </div>
+</main>
