b6c03b1631a1f908d84fce024868cab6a52b0693
ythknakamura <ythk.nakamura@gmail.com>
2024-08-12 18:21:20 +0900

    chap8まで

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   import AnswerButton from "./AnswerButton.svelte";
   import {getQuizdata} from "./quizdataFactory";
+  import TitlePage from "./TitlePage.svelte";
+  let showTitle = true;
   let currentScore = 0;
   let renzokuSeikai = 0;
   let quizdata = getQuizdata();
@@ -17,20 +19,24 @@
   }
 </script>
 
-<main class="flex flex-col h-svh">
-  <!-- メニューバー -->
-  <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
-    <div>スコア:{currentScore}</div>
-    <div>連続正解数:{renzokuSeikai}</div>
-  </div>
-  
-  <!-- 問題 -->
-  <div class="bg-green-200 text-center text-4xl py-4">{quizdata.mondai}</div>
-  
-  <!-- 選択肢 -->
-  <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
-    {#each quizdata.taku as t}
-      <AnswerButton on:click={() => answerButtonClicked(quizdata.seikai===t)}>{t}</AnswerButton>
-    {/each}
-  </div>
-</main>
+{#if showTitle}
+  <TitlePage on:click={()=>showTitle=false}></TitlePage>
+{:else}
+  <main class="flex flex-col h-svh">
+    <!-- メニューバー -->
+    <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
+      <div>スコア:{currentScore}</div>
+      <div>連続正解数:{renzokuSeikai}</div>
+    </div>
+    
+    <!-- 問題 -->
+    <div class="bg-green-200 text-center text-4xl py-4">{quizdata.mondai}</div>
+    
+    <!-- 選択肢 -->
+    <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
+      {#each quizdata.taku as t}
+        <AnswerButton on:click={() => answerButtonClicked(quizdata.seikai===t)}>{t}</AnswerButton>
+      {/each}
+    </div>
+  </main>
+{/if}
---
+++ src/TitlePage.svelte
@@ -0,0 +1,11 @@
+<div class="bg-[url('/src/assets/fish.jpg')] bg-[size:100%] bg-repeat flex flex-col justify-center items-center h-svh gap-16">
+    <div class="text-7xl font-extrabold backdrop-blur-sm bg-white/20 p-4 rounded-2xl">
+        Quiz App
+    </div>
+    <div class="text-4xl font-extrabold">
+        魚へんの漢字
+    </div>
+    <button on:click class="bg-blue-700 text-white font-bold text-3xl rounded-full border-2 p-4 w-1/2">
+        スタート
+    </button>
+</div>
