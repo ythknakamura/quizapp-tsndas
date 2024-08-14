c0deb6e0fc88ed5937bc035d4f97e04564ddf10e
ythknakamura <ythk.nakamura@gmail.com>
2024-08-15 00:01:03 +0900

    chap14書き途中

--- src/GameoverModal.svelte
+++ src/GameoverModal.svelte
@@ -1,6 +1,7 @@
 <script>
     let modal;
     let score;
+    let name;
     export function showModal(currentScore){
         score = currentScore;
         modal.showModal();
@@ -15,6 +16,7 @@
         <div class="text-xl font-bold">
             スコア:{score}
         </div>
+        <input bind:value={name} class="p-3 border-none focus:outline-none rounded-xl w-full" type="text" placeholder="名前の入力"/>
         <button on:click class="text-xl w-1/2 border-2 border-gray-500 rounded-xl">OK</button>
     </div>
 </dialog>
